b822d869edbfa06accea87bf9adff61ef6177c28
kshitijaroraa12 <75884399+kshitijaroraa12@users.noreply.github.com>
2020-12-12 17:06:15 +0530

    Add files via upload

---
+++ fi.java
@@ -0,0 +1,28 @@
+class Test
+{
+   public static void main(String args[])
+   {
+     int i,j,k;
+     for(i=0;i<7;i++)
+       {
+         for(j=5;j>=i;j--)
+           {
+             System.out.print(" ");
+           }
+             for(k=1;k<(i*2);k++)
+            {
+                if(k>1 &&k<(i*2)-1)
+                 {
+                    System.out.print(" ");
+
+                  }
+               else
+             {
+               System.out.print("*");
+             }
+          }
+              System.out.println();
+              }
+          }
+} 
+              
